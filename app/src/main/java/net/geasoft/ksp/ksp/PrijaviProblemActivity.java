81304d86bef099b75f79682a626549d15099ffa5
Boskule <32804166+Boskule@users.noreply.github.com>
2017-11-02 21:32:15 +0100

    Dodato dodavanje.

--- app/src/main/java/net/geasoft/ksp/ksp/PrijaviProblemActivity.java
+++ app/src/main/java/net/geasoft/ksp/ksp/PrijaviProblemActivity.java
@@ -1,12 +1,17 @@
 package net.geasoft.ksp.ksp;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PrijaviProblemActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -15,8 +20,7 @@ public class PrijaviProblemActivity extends AppCompatActivity  implements View.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prijavi_problem);
 
-        Toolbar tulbar =
-                (Toolbar) findViewById(R.id.toolbar_prijava);
+        Toolbar tulbar = (Toolbar) findViewById(R.id.toolbar_prijava);
         setSupportActionBar(tulbar);
 
         // Get a support ActionBar corresponding to this toolbar
@@ -34,6 +38,30 @@ public class PrijaviProblemActivity extends AppCompatActivity  implements View.O
         Button dugme = (Button)findViewById(R.id.button3);
         if(boks.isChecked())  dugme.setVisibility(View.GONE);
         else  dugme.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.meni_prijava, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.prijava_okej) {
+            ProblemModel problem = new ProblemModel();
+            problem.id_korisnika = 69;
+            problem.id_vrste = 69;
+            problem.opis = ((EditText)findViewById(R.id.editText)).getText().toString();
+            problem.slika = "Nema";
+            problem.opstina = "Bogac";
+            problem.latitude = "44";
+            problem.longitude = "44";
+            ProblemMetode.Dodaj(problem, this);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/net/geasoft/ksp/ksp/ProblemModel.java
@@ -0,0 +1,14 @@
+package net.geasoft.ksp.ksp;
+
+
+public class ProblemModel {
+
+    public int id;
+    public int id_vrste;
+    public int id_korisnika;
+    public String opis;
+    public String slika;
+    public String opstina;
+    public String latitude;
+    public String longitude;
+}
