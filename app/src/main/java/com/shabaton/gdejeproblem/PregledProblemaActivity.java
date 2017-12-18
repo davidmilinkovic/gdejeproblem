e15445f7184a061251f811cfd84887449bcf7d58
David <david.milinkovic@gmail.com>
2017-12-18 04:09:14 +0100

    TASTATURA KONACNO RADI KAKO TREBA

--- app/src/main/java/com/shabaton/gdejeproblem/PregledProblemaActivity.java
+++ app/src/main/java/com/shabaton/gdejeproblem/PregledProblemaActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -108,5 +109,14 @@ public class PregledProblemaActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return true;
     }
 }
